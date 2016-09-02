6f9604ce91a5c9a1f143018943bf9748b96111b7
ryukocn2008 <ryukocn2008@yahoo.co.jp>
2016-09-01 22:18:35 -0700

    Test VS 2015 for WinForm

---
+++ TestWinFormApp/TestWinFormApp/MainForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestWinFormApp
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHelloWinForm_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello, World!");
+        }
+    }
+}
